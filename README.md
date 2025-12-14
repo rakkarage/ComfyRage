e0d0f10723503c10a1783ce3c0ba66a82761a116
Rakka Rage <rakkarage@gmail.com>
2025-12-14 02:01:20 -0500

    s

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # ComfyRage
 
+Install: clone into custom_nodes. 
+
 ![Screenshot](screenshot.png)
 
 ```
 {dog, {leash|}|cat|horse}, [simple_background] // Pre strips comments, expands random, and expands de-emphasis, Show + Debug proves it!
 ```
 
-# Pre
+## Pre
+
 - Strips comments: `/* comment1 */ tag1, tag2, // comment2`
 - Expands random: `{tag1|tag2|etc}, {tag4|}`
 - Expands de-emphasis: `[less[less]]`
 
 Can use show or debug if you wanna see, or just pass to encoder.
 
-# Show
+## Show
+
 - Shows text, optionally passes on input as output.
 
-# Debug
+## Debug
+
 - Show weights, optionally passes on input as output.
 
 Thanks!
--- __init__.py
+++ __init__.py
@@ -7,14 +7,17 @@ NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
 # Import from nodes/ subdirectory
-from .nodes.Show import NODE_CLASS_MAPPINGS as show_map
+from .nodes.Show import NODE_CLASS_MAPPINGS as show_map, NODE_DISPLAY_NAME_MAPPINGS as show_display_map
 NODE_CLASS_MAPPINGS.update(show_map)
+NODE_DISPLAY_NAME_MAPPINGS.update(show_display_map)
 
-from .nodes.Debug import NODE_CLASS_MAPPINGS as debug_map
+from .nodes.Debug import NODE_CLASS_MAPPINGS as debug_map, NODE_DISPLAY_NAME_MAPPINGS as debug_display_map
 NODE_CLASS_MAPPINGS.update(debug_map)
+NODE_DISPLAY_NAME_MAPPINGS.update(debug_display_map)
 
-from .nodes.Pre import NODE_CLASS_MAPPINGS as pre_map
+from .nodes.Pre import NODE_CLASS_MAPPINGS as pre_map, NODE_DISPLAY_NAME_MAPPINGS as pre_display_map
 NODE_CLASS_MAPPINGS.update(pre_map)
+NODE_DISPLAY_NAME_MAPPINGS.update(pre_display_map)
 
 WEB_DIRECTORY = os.path.join(os.path.dirname(__file__), "web")
 
