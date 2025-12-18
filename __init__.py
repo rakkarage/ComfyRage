838a487426cd0ca2eb786341c97ec82a1f221f73
Rakka Rage <rakkarage@gmail.com>
2025-12-18 07:37:29 -0500

    z

--- __init__.py
+++ __init__.py
@@ -5,18 +5,30 @@ import os
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
-from .nodes.Pre import NODE_CLASS_MAPPINGS as pre_map, NODE_DISPLAY_NAME_MAPPINGS as pre_display_map
+from .nodes.Pre import (
+    NODE_CLASS_MAPPINGS as pre_map,
+    NODE_DISPLAY_NAME_MAPPINGS as pre_display_map,
+)
+
 NODE_CLASS_MAPPINGS.update(pre_map)
 NODE_DISPLAY_NAME_MAPPINGS.update(pre_display_map)
 
-from .nodes.Show import NODE_CLASS_MAPPINGS as show_map, NODE_DISPLAY_NAME_MAPPINGS as show_display_map
+from .nodes.Show import (
+    NODE_CLASS_MAPPINGS as show_map,
+    NODE_DISPLAY_NAME_MAPPINGS as show_display_map,
+)
+
 NODE_CLASS_MAPPINGS.update(show_map)
 NODE_DISPLAY_NAME_MAPPINGS.update(show_display_map)
 
-from .nodes.Debug import NODE_CLASS_MAPPINGS as debug_map, NODE_DISPLAY_NAME_MAPPINGS as debug_display_map
+from .nodes.Debug import (
+    NODE_CLASS_MAPPINGS as debug_map,
+    NODE_DISPLAY_NAME_MAPPINGS as debug_display_map,
+)
+
 NODE_CLASS_MAPPINGS.update(debug_map)
 NODE_DISPLAY_NAME_MAPPINGS.update(debug_display_map)
 
 WEB_DIRECTORY = os.path.join(os.path.dirname(__file__), "web")
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
