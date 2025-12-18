78f6196fffe88d92653e8e9968ab12be3eee5b19
Rakka Rage <rakkarage@gmail.com>
2025-12-18 07:36:43 -0500

    z

--- nodes/Util.py
+++ nodes/Util.py
@@ -1,10 +1,12 @@
 # ComfyUI/custom_nodes/ComfyRage/nodes/Util.py
 
+
 def extract(kwargs):
     if "string" not in kwargs:
         return []
 
     return [str(val) for val in kwargs["string"]]
+
 
 def inject(values, unique_id, extra_pnginfo):
     if not extra_pnginfo:
@@ -15,10 +17,7 @@ def inject(values, unique_id, extra_pnginfo):
         return
 
     workflow = info["workflow"]
-    node = next(
-        (x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]),
-        None
-    )
+    node = next((x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None)
 
     if node:
         node["widgets_values"] = [values]
