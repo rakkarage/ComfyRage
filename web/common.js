41346d6df540db82aa27a7ad58f919823177bab7
Rakka Rage <rakkarage@gmail.com>
2025-12-18 08:03:56 -0500

    p

--- web/common.js
+++ web/common.js
@@ -10,9 +10,7 @@ export class ComfyRageCommon {
             async beforeRegisterNodeDef(nodeType, nodeData) {
                 if (nodeData.name !== nodeName) return;
 
-                // Reusable populate function (copied from Easy-Use)
                 function populate(text, name = 'text') {
-                    // Remove all widgets from 'name' onward
                     if (this.widgets) {
                         const pos = this.widgets.findIndex(w => w.name === name);
                         if (pos !== -1) {
@@ -23,16 +21,13 @@ export class ComfyRageCommon {
                         }
                     }
 
-                    // Create one read-only widget per line (text is expected to be an array)
-                    let lines = Array.isArray(text) ? text : [text];
-                    for (const line of lines) {
+                    for (const line of text) {
                         const w = ComfyWidgets.STRING(this, name, ["STRING", { multiline: true }], app).widget;
                         w.inputEl.readOnly = true;
                         w.inputEl.style.opacity = 0.6;
                         w.value = line;
                     }
 
-                    // Auto-resize node
                     requestAnimationFrame(() => {
                         const sz = this.computeSize();
                         if (sz[0] < this.size[0]) sz[0] = this.size[0];
@@ -42,37 +37,29 @@ export class ComfyRageCommon {
                     });
                 }
 
-                // On execution: update display
                 const onExecuted = nodeType.prototype.onExecuted;
                 nodeType.prototype.onExecuted = function (message) {
                     onExecuted?.apply(this, arguments);
                     if (message?.text !== undefined) {
-                        // Easy-Use expects message.text to be an ARRAY (even for single value)
-                        let display = Array.isArray(message.text) ? message.text : [message.text];
-                        populate.call(this, display, 'text');
+                        populate.call(this, message.text, 'text');
                     }
                 };
 
-                // On load: restore from widgets_values
                 const onConfigure = nodeType.prototype.onConfigure;
                 nodeType.prototype.onConfigure = function () {
                     onConfigure?.apply(this, arguments);
                     if (this.widgets_values?.length) {
-                        // widgets_values is [ ["line1", "line2"] ] or ["single"]
-                        let values = this.widgets_values[0];
-                        if (!Array.isArray(values)) values = [values];
+                        let values = this.widgets_values[0]; // TODO: loop?
                         populate.call(this, values, 'text');
                     }
                 };
 
-                // On serialize: save current widget values
                 const serialize = nodeType.prototype.serialize;
                 nodeType.prototype.serialize = function () {
                     const orig = serialize ? serialize.apply(this, arguments) : {};
-                    // Collect all 'text' widget values
                     const textWidgets = this.widgets?.filter(w => w.name === 'text') || [];
                     const values = textWidgets.map(w => w.value);
-                    return { ...orig, widgets_values: [values] }; // Save as [["text"]] like Easy-Use
+                    return { ...orig, widgets_values: [values] };
                 };
             }
         };
